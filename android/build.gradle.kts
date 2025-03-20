967547c2bd3218c467df2498ac175901d0e7ba69
viktor <v.scherbyna@tinqin.com>
2025-03-20 21:35:43 +0200

    android SDK version to 2.0.0

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,17 +1,6 @@
 import java.nio.file.Files
 import java.util.Properties
 
-/*allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-        maven {
-            url = uri("https://jitpack.io")
-        }
-    }
-}*/
-
 rootProject.buildDir = file("../build")
 
 subprojects {
@@ -64,7 +53,7 @@ android {
 }
 
 dependencies {
-    implementation("com.github.ottuco:ottu-flutter-android:1.0.1")
+    implementation("com.github.ottuco:ottu-flutter-android:2.0.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
     implementation("com.squareup.moshi:moshi-adapters:1.15.1")
     implementation("com.squareup.moshi:moshi-kotlin-codegen:1.15.1")
