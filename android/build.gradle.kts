f082372c797744957fe06e3da2aa74bd3cea3878
viktor <v.scherbyna@tinqin.com>
2025-08-05 16:52:42 +0300

    fix version to 2.1.3

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -53,6 +53,12 @@ android {
 }
 
 dependencies {
-    implementation("com.github.ottuco:ottu-flutter-android:2.1.2")
-
+    implementation("com.github.ottuco:ottu-flutter-android:2.1.4")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
+    implementation("com.squareup.moshi:moshi-adapters:1.15.1")
+    implementation("com.squareup.moshi:moshi-kotlin-codegen:1.15.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation ("com.google.android.material:material:1.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
 }
