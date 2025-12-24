1118da62db57745cc83b01f702c0022ef431311a
vscmaster <victor.scherbyna@gmail.com>
2025-12-24 12:05:10 +0200

    MS-311: update Koin to 4.1.1 (#12)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -21,7 +21,7 @@ val flutterVersionName = localProperties.getProperty("flutter.versionName") ?: "
 plugins {
     id("com.android.library")
     kotlin("android")
-    kotlin("plugin.serialization") version "2.1.0"
+    kotlin("plugin.serialization") version "2.2.20"
 }
 
 group = "com.ottu.flutter.checkout"
@@ -70,12 +70,12 @@ allprojects {
 }
 
 dependencies {
-    implementation("com.github.ottuco:ottu-android-checkout:2.1.5")
-    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
-    implementation("com.squareup.moshi:moshi-adapters:1.15.1")
-    implementation("com.squareup.moshi:moshi-kotlin-codegen:1.15.1")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("com.github.ottuco:ottu-android-checkout:2.1.6")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
+    implementation("com.squareup.moshi:moshi-adapters:1.15.2")
+    implementation("com.squareup.moshi:moshi-kotlin-codegen:1.15.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.6")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
 }
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -40,8 +40,8 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.library") version "8.8.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("com.android.library") version "8.9.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 
 // Check if :ottu-android-checkout is included as a dependency
