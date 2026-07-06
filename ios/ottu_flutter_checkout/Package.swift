184ca62b6262501fea4b911a9bb03e62ec66ffab
ihorbenko <ihorbenko@plexteq.com>
2026-07-06 10:36:07 +0300

    Multiple iOS native frameworks Swift package

--- ios/ottu_flutter_checkout/Package.swift
+++ ios/ottu_flutter_checkout/Package.swift
@@ -1,6 +1,4 @@
-// swift-tools-version: 5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version: 5.7
 import PackageDescription
 
 let package = Package(
@@ -8,16 +6,40 @@ let package = Package(
     defaultLocalization: "en",
     platforms: [.iOS(.v15)],
     products: [
-        .library(name: "ottu-flutter-checkout", targets: ["ottu_flutter_checkout"])
+        .library(
+            name: "ottu-flutter-checkout",
+            targets: ["ottu_flutter_checkout"]
+        ),
+        .library(
+            name: "ottu-flutter-checkout-sentry",
+            targets: ["ottu_flutter_checkout_sentry"]
+        )
     ],
     dependencies: [
-         .package(name: "ottu_checkout_sdk", url: "https://github.com/ottuco/ottu-ios", from: "2.2.12")
+        .package(
+            name: "ottu_checkout_sdk",
+            url: "https://github.com/ottuco/ottu-ios",
+            from: "2.2.12-no-deps-8"
+        )
     ],
     targets: [
         .target(
             name: "ottu_flutter_checkout",
             dependencies: [
-                .product(name: "ottu_checkout_sdk", package: "ottu_checkout_sdk"),
+                .product(name: "ottu_checkout_sdk", package: "ottu_checkout_sdk")
+            ],
+            resources: [
+                .process("PrivacyInfo.xcprivacy"),
+                .process("Resources")
+            ],
+            cSettings: [
+                .headerSearchPath("include/ottu_flutter_checkout")
+            ]
+        ),
+        .target(
+            name: "ottu_flutter_checkout_sentry",
+            dependencies: [
+                .product(name: "ottu_checkout_sdk_sentry", package: "ottu_checkout_sdk")
             ],
             resources: [
                 .process("PrivacyInfo.xcprivacy"),
